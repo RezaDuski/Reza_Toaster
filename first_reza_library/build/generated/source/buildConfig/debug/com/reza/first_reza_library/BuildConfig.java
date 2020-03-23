a83aad7da7db6abdec17593021833c255d9cf914
RezaDuski <62575397+RezaDuski@users.noreply.github.com>
2020-03-24 02:17:35 +0300

    Add files via upload

---
+++ first_reza_library/build/generated/source/buildConfig/debug/com/reza/first_reza_library/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.reza.first_reza_library;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.reza.first_reza_library";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.reza.first_reza_library";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
